d3281148453550dbee52035a432f6a46a9b32d9b
Odravison <odravison.amaral@dce.ufpb.br>
2016-06-01 02:14:37 -0300

    Aperfeiçoamento na idependencia do script.

--- installing-docker.sh
+++ installing-docker.sh
@@ -1,8 +1,23 @@
+#!/bin/sh
+
 sudo apt-get update -y
 sudo apt-get install apt-transport-https ca-certificates -y
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
+#Verificando se o arquivo existe ou não.
+if [ -s /etc/apt/sources.list.d/docker.list ]; then
+	echo "=================>>>> LOG: Arquivo existia, deletando arquivo por completo e adicionando  novo arquivo"
+	sudo rm -rf /etc/apt/sources.list.d/docker.list
+else
+	echo "=================>>>> LOG: Arquivo não existia, adicionando arquivo"
+
+fi
+
+#E finalmente, criando o arquivo e adicionando um valor dentro dele.
 sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
+
+
+
 
 sudo apt-get update -y
 
