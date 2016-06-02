bd34a4784d9adb4b4427e7f80314fbd2876a6726
Rennan Felizardo <rennan.felizardo@dce.ufpb.br>
2016-06-01 23:48:32 -0300

    Versão atualizada do docker com echos

--- DockerComposeInstallUbuntu.sh
+++ DockerComposeInstallUbuntu.sh
@@ -2,5 +2,8 @@
 sudo -i
 
 curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-
+echo "Instalando..."
+echo "Concedendo Permissões"
 chmod +x /usr/local/bin/docker-compose
+
+echo "Docker Compose instalado com sucesso!"
