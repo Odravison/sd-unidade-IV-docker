6f1074e854f1779cd023a3ca2bf4e59c546933b5
Rennan Felizardo <rennan.felizardo@dce.ufpb.br>
2016-06-01 23:35:46 -0300

    Versão atualizada do docker compose

--- DockerComposeInstallUbuntu.sh
+++ DockerComposeInstallUbuntu.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+sudo -i
+
 curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
